ab677b74f609f17b2743210f31d8670d116b2cbd
Elviis19 <elvisjosepena015@gmail.com>
2024-10-09 14:50:51 -0500

    commit medicamentos

---
+++ src/main/Ejecutar.java
@@ -0,0 +1,19 @@
+package main;
+
+import Medicamentos.Medicamentos;
+
+public class Ejecutar {
+
+    public static void main(String[] args) {
+
+       Medicamentos medicamento = new Medicamentos("Aspirina", "Bayer", "Oral");
+       Medicamentos.Posologia posologia = medicamento.new Posologia("Adultos y "
+                + "mayores de " + "16 años", 500, "6 Horas",
+                "No debe tomar este medicamento" + " con el estómago vacío");
+        medicamento.setPosologia(posologia);
+        medicamento.mostrarDatos();
+        posologia.mostrarDatos();
+
+    }
+
+}
